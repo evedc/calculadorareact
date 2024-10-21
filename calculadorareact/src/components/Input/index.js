a0d1cf031e15a0c93c137f35a93b72d0ee839956
Évelim Dornelles <dorrrnelles@gmail.com>
2024-10-21 09:57:30 -0300

    final

--- calculadorareact/src/components/Input/index.js
+++ calculadorareact/src/components/Input/index.js
@@ -1,12 +1,12 @@
-import { InputContainer } from "./styles";
 
-const Input = ({ value }) => {
-  return (
-    <InputContainer>
-      {/* Input desabilitado com valor vindo das props */}
-      <input disabled value={value} />
-    </InputContainer>
-  );
-}
+import { InputContainer } from './styles';
 
-export default Input;
+const Input = ({value}) => {
+    return (
+      <InputContainer>
+       <input disabled value={value}/>
+      </InputContainer>
+    );
+  }
+  
+  export default Input;
--- calculadorareact/src/components/Input/styles.js
+++ calculadorareact/src/components/Input/styles.js
@@ -1,30 +1,29 @@
 import styled from 'styled-components';
 
 export const InputContainer = styled.div`
-width: 100%.
-height: 75px;
+    width: 100%;
+    height: 75px;
+    background-color: #AAAAFF;
 
-background-color: #AAFFAA;
-display: flex;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
 
-align-items: center;
-justify-content: flex-end;
+    font-size: 24px;
+    font-family: 'Roboto';
+    
 
-font-size: 24px;
-font-family: 'Roboto';
-
-
-input {
-width: 100%;
-height: 75px;
-background-color: #AAFFAA;
-border: 0;
-flex-direction: column;
-align-items: flex-end;
-align: right;
-padding: 0 10px;
-font-size: 24px;
-font-family: 'Roboto';
-color: #FFFFFF;
-}
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #AAAAFF;
+        border: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        padding: 0 10px;
+        font-size: 24px;
+        font-family: 'Roboto';
+        color: #FFFFFF;
+    }
 `
